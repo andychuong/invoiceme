b172d0469c52bfa761e8e342767cf9ca5297813c
Andy C <achuongdev@gmail.com>
2025-11-09 16:50:26 -0600

    Add root page redirect to signup

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,28 +1,5 @@
-'use client';
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { authService } from '@/services/authService';
-import { Box } from '@chakra-ui/react';
-import { LoadingSpinner } from '@/components/common/LoadingSpinner';
+import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const checkAuth = () => {
-      if (authService.isAuthenticated()) {
-        router.push('/customers');
-      } else {
-        router.push('/login');
-      }
-    };
-    checkAuth();
-  }, [router]);
-
-  return (
-    <Box display="flex" alignItems="center" justifyContent="center" minH="100vh">
-      <LoadingSpinner />
-    </Box>
-  );
+  redirect('/signup');
 }
